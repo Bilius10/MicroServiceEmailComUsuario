c1c73e3ec06e0fcac952a3d726b69e177d4303d0
Bilius10 <jvdrdo@gmail.com>
2024-11-13 21:00:51 -0300

    Resolvendo conflitos após o pull

---
+++ user/src/main/java/com/ms/user/InfraSecurity/TokenService.java
@@ -0,0 +1,4 @@
+package com.ms.user.InfraSecurity;
+
+public class TokenService {
+}
--- user/src/main/java/com/ms/user/dtos/LoginWithCode.java
+++
@@ -1,6 +0,0 @@
-package com.ms.user.dtos;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginWithCode(@NotBlank String name, @NotBlank String senha, @NotBlank String codeTemporario) {
-}
